fab6d3a484982f653585a3879de3beb292dbfd5a
atvcaptain <captain@web.de>
2014-01-18 13:12:31 +0100

    update test.py

--- BoxBranding/lib/test.py
+++ BoxBranding/lib/test.py
@@ -1,9 +1,9 @@
 import boxbranding
 
-print boxbranding.getBrand()
+print boxbranding.getMachineBrand()
 print boxbranding.getMachineName()
 print boxbranding.getBoxType()
 print boxbranding.getOEM()
 print boxbranding.getDriverDate()
-print boxbranding.getVersion()
-print boxbranding.getBuild()
+print boxbranding.getImageVersion()
+print boxbranding.getImageBuild()
