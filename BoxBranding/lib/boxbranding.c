eaba40a24bc82b61f984315eec798f8a805b7ad2
a4tech <darezik@gmail.com>
2014-09-11 15:53:23 +0200

    [Hde2] Box picutres updates and some preparation for future use

--- BoxBranding/lib/boxbranding.c
+++ BoxBranding/lib/boxbranding.c
@@ -93,23 +93,7 @@ const char *_getBoxType()	// this will try to find the correct BOX MACHINE e.x M
 				return "xpeedlx1";
 			}
 		}
-	}
-	else if(strcmp(BOXTYPE, "atemio6x00") == 0)
-	{
-		boxtype_name = ReadProcEntry("/proc/stb/info/boxtype");
-		if(strcmp(boxtype_name, "ini-2000am") == 0) 
-		{
-			boxtype_name = ReadProcEntry("/proc/stb/fp/version");
-			if(startsWith(boxtype_name, "2"))
-			{
-				return "atemio6200";
-			}
-			else
-			{
-				return "atemio6100";
-			}
-		}
-	}	  
+	}  
 	else if(strcmp(BOXTYPE, "ventonhdx") == 0)
 	{
 		boxtype_name = ReadProcEntry("/proc/stb/info/boxtype");
@@ -169,18 +153,6 @@ const char *_getMachineName()
 		else if(strcmp(boxtype_name, "ini-1000lx") == 0) 
 		{
 			return "LX-2T";
-		}
-		else if(strcmp(boxtype_name, "ini-2000am") == 0)
-		{
-			boxtype_name = ReadProcEntry("/proc/stb/fp/version");
-			if(startsWith(boxtype_name, "2"))
-			{
-				return "6200";
-			}
-			else
-			{
-				return "6100";
-			}
 		}
 		/** XTREND DETECTION */
 		else if(strcmp(boxtype_name, "et4x00") == 0) 
