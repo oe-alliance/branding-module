b4aed9c79ffa5f738624cabd989e400b3740f401
atvcaptain <captain@web.de>
2014-10-10 11:19:42 +0200

    add dm7080

--- BoxBranding/RcModel.py
+++ BoxBranding/RcModel.py
@@ -56,7 +56,7 @@ class RcModel:
 			remotefolder = 'dmm0'
 		elif getBoxType() in ('dm800', 'dm800se', 'dm500hd'):
 			remotefolder = 'dmm1'
-		elif getBoxType() in ('dm7020hd', 'dm7020hdv2', 'dm800sev2', 'dm500hdv2'):
+		elif getBoxType() in ('dm7080', 'dm7020hd', 'dm7020hdv2', 'dm800sev2', 'dm500hdv2'):
 			remotefolder = 'dmm2'
 		elif getBoxType() in ('tmtwin', 'tm2t'):
 			remotefolder = 'tm1'
