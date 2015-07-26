b2013c1480a722c25643e9b52f33ffb14d29f54b
Captain <captain.onboard@web.de>
2015-07-26 20:00:39 +0200

    add tmnanosem2

--- BoxBranding/RcModel.py
+++ BoxBranding/RcModel.py
@@ -96,7 +96,7 @@ class RcModel:
 			remotefolder = 'tm1'
 		elif boxtype in ('tmsingle', 'tmnano', 'tmnano2t', 'tmnano3t', 'tmnano2super'):
 			remotefolder = 'tm2'
-		elif boxtype in ('tmnanose', 'tmnanosecombo'):
+		elif boxtype in ('tmnanose', 'tmnanosecombo', 'tmnanosem2'):
 			remotefolder = 'tm3'
 		elif boxtype == 'uniboxhd1':
 			fp_version = str(getFPVersion())
