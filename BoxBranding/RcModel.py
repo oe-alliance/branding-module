630752dee8d505bf474762c005aede03e284910a
Captain <captain.onboard@web.de>
2015-08-08 09:58:29 +0200

    add E4HD

--- BoxBranding/RcModel.py
+++ BoxBranding/RcModel.py
@@ -54,6 +54,8 @@ class RcModel:
 			remotefolder = 'red1'
 		elif boxtype in ('odin2hybrid'):
 			remotefolder = 'ax1'
+		elif boxtype in ('e4hd'
+			remotefolder = 'e4hd'
 		elif boxtype in ('enibox'):
 			remotefolder = 'hdbox'
 		elif boxtype in ('mago'):
