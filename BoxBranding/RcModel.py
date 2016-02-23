2231e81c81a1bb1e12a13033fa3710e5deaec2b9
captain <openatv@gmail.com>
2016-02-23 13:16:10 +0100

    add TM-NANO-SE Plus

--- BoxBranding/RcModel.py
+++ BoxBranding/RcModel.py
@@ -110,7 +110,7 @@ class RcModel:
 			remotefolder = 'tm2'
 		elif boxtype in ('tmnanose', 'tmnanosecombo'):
 			remotefolder = 'tm3'
-		elif boxtype in ('tmnanosem2'):
+		elif boxtype in ('tmnanosem2', 'tmnanoseplus'):
 			remotefolder = 'tm4'
 		elif boxtype == 'uniboxhd1':
 			fp_version = str(getFPVersion())
