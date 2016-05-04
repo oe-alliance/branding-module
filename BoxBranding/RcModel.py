2dcac21941927f498d693d8f7a9a1dfbb81de06e
captain <openatv@gmail.com>
2016-05-04 22:15:11 +0200

    add hd51 oem

--- BoxBranding/RcModel.py
+++ BoxBranding/RcModel.py
@@ -86,7 +86,7 @@ class RcModel:
 			remotefolder = 'gb0'
 		elif boxtype == 'gbquadplus':
 			remotefolder = 'gb1'
-		elif boxtype in ('mutant1100', 'mutant1200', 'mutant500c', 'vizyonvita', 'mutant1265', 'mutant1500'):
+		elif boxtype in ('mutant1100', 'mutant1200', 'mutant500c', 'vizyonvita', 'mutant1265', 'mutant1500', 'mutant51', 'ax51'):
 			remotefolder = 'hd1100'
 		elif boxtype in ('mutant2400', 'quadbox2400'):
 			remotefolder = 'hd2400'
