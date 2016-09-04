a35a051af4c5f81f2f24514f8e243b5853cca5ef
captain <openatv@gmail.com>
2016-09-04 10:08:56 +0200

    add mutant530c

--- BoxBranding/RcModel.py
+++ BoxBranding/RcModel.py
@@ -96,7 +96,7 @@ class RcModel:
 			remotefolder = 'gb0'
 		elif boxtype == 'gbquadplus':
 			remotefolder = 'gb1'
-		elif boxtype in ('mutant11', 'mutant1100', 'mutant1200', 'mutant500c', 'vizyonvita', 'mutant1265', 'mutant1500', 'mutant51'):
+		elif boxtype in ('mutant11', 'mutant1100', 'mutant1200', 'mutant530c', 'mutant500c', 'vizyonvita', 'mutant1265', 'mutant1500', 'mutant51'):
 			remotefolder = 'hd1100'
 		elif boxtype in ('mutant2400', 'quadbox2400'):
 			remotefolder = 'hd2400'
