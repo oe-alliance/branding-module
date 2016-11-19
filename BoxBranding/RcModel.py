e949ea5e2fee23286a1c86eba6b30a995bdc5337
captain <openatv@gmail.com>
2016-11-19 15:55:37 +0100

    add bre2ze4k dummy real infos later

--- BoxBranding/RcModel.py
+++ BoxBranding/RcModel.py
@@ -58,7 +58,7 @@ class RcModel:
 			remotefolder = 'evo6'
 		elif boxtype in ('novacombo', 'novatwin'):
 			remotefolder = 'evo7'
-		elif boxtype in ('bre2ze'):
+		elif boxtype in ('bre2ze','bre2ze4k'):
 			remotefolder = 'wwio1'
 		elif boxtype in ('twinboxlcd'):
 			remotefolder = 'red1'
