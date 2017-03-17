c56e2b30e08100398c2073b7e87449092264d839
captain <openatv@gmail.com>
2017-03-17 09:08:49 +0100

    add zgemma h3.2tc

--- BoxBranding/RcModel.py
+++ BoxBranding/RcModel.py
@@ -92,7 +92,7 @@ class RcModel:
 			remotefolder = 'zgemma1'
 		elif boxtype in ('zgemmash2', 'zgemmass', 'zgemmaslc'):
 			remotefolder = 'zgemma2'
-		elif boxtype in ('zgemmahs', 'zgemmah2s', 'zgemmah2h','zgemmah5','zgemmah52s','zgemmah3ac','zgemmah5ac','zgemmah52tc'):
+		elif boxtype in ('zgemmahs', 'zgemmah2s', 'zgemmah2h','zgemmah5','zgemmah52s','zgemmah3ac','zgemmah5ac','zgemmah52tc','zgemmah32tc'):
 			remotefolder = 'zgemma3'
 		elif boxtype in ('zgemmai55'):
 			remotefolder = 'zgemma5'
