d886ee0211af0f0ca8c988eba75f8a2f13260a29
captain <openatv@gmail.com>
2017-10-09 14:11:32 +0200

    add qviart lunix and lunix4k

--- BoxBranding/RcModel.py
+++ BoxBranding/RcModel.py
@@ -149,6 +149,8 @@ class RcModel:
 			remotefolder = 'iqon2'
 		elif boxtype in ('force3uhdplus','force3uhd'):
 			remotefolder = 'iqon3'
+		elif boxtype in ('lunix34k','lunix'):
+			remotefolder = 'qviart1'
 		elif boxtype in ('formuler1', 'formuler3', 'formuler4', 'formuler4turbo', 'formuler1tc', 'formuler3ip', 'formuler4ip'):
 			remotefolder = 'formuler1'
 		elif boxtype in ('triplex', 'ultrabox'):
