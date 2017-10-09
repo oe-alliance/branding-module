1eb979b514dcf6f8a5763d1a7151b5a249d82dc2
captain <openatv@gmail.com>
2017-10-09 14:51:01 +0200

    add tiviar alpha

--- BoxBranding/RcModel.py
+++ BoxBranding/RcModel.py
@@ -89,7 +89,7 @@ class RcModel:
 			remotefolder = 'evo7'
 		elif boxtype in ('bre2ze','bre2ze4k','bre2zet2c'):
 			remotefolder = 'wwio1'
-		elif boxtype in ('tiviarmin'):
+		elif boxtype in ('tiviarmin','tiviaralpha'):
 			remotefolder = 'tiviar'
 		elif boxtype in ('twinboxlcd'):
 			remotefolder = 'red1'
