6e1f7405bccc148dfb2c6e7b6d54ccc4aad719a4
Captain <captain.onboard@web.de>
2018-11-02 15:45:05 +0100

    add 9920lx

--- BoxBranding/RcModel.py
+++ BoxBranding/RcModel.py
@@ -126,7 +126,7 @@ class RcModel:
 			remotefolder = 'visionnet'
 		elif boxtype in ('9900lx'):
 			remotefolder = 'protek1'
-		elif boxtype in ('9910lx','9911lx','protek4k'):
+		elif boxtype in ('9910lx','9911lx','protek4k','9920lx'):
 			remotefolder = 'protek2'
 		elif boxtype in ('zgemmash1', 'zgemmas2s'):
 			remotefolder = 'zgemma1'
