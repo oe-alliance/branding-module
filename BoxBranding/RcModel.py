70ac4b5586e934876493f9b76b0766431ca69f72
Captain <captain.onboard@web.de>
2022-05-28 08:52:19 +0200

    add sfx6018

--- BoxBranding/RcModel.py
+++ BoxBranding/RcModel.py
@@ -262,6 +262,7 @@ class RcModel:
 			"sf8008t": "sf8008",
 			"sf8008opt": "sf8008",
 			"sfx6008": "octagon3",
+			"sfx6018": "octagon3",
 			"sx988": "octagon2",
 			"sf98": "sf98",
 			"singleboxlcd": "red2",
